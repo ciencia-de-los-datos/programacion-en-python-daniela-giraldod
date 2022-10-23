85e48f8dbee6e81c94cb13ef0eb64520eb4521fc
daniela-giraldod <112145954+daniela-giraldod@users.noreply.github.com>
2022-10-23 18:06:07 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -48,17 +48,8 @@ def pregunta_02():
     ]
 
     """
-    datos1 = [ row[0] for row in datos]
-
-    from collections import Counter
-
-    resultado = Counter(datos1)
-    resultado = list(resultado.items()) #convertir diccionario a listas de tuplas
-
-    from operator import itemgetter
-    resultado.sort(key=itemgetter(0), reverse=False)  #ordenar de acuerdo a llave
-
-    return  resultado
+    
+    return  
 
 
 def pregunta_03():
@@ -76,29 +67,9 @@ def pregunta_03():
     ]
 
     """
-    with open("data.csv", "r") as file:
-    datos = file.readlines() 
 
-    datos = [row.replace("\n", "")for row in datos]
-    datos = [row.split("\t")for row in datos]
-
-    datos1 = [(row[0], row[1]) for row in datos]
-
-    contador = {}
-    for key,value in datos1:
-        if key in contador:
-        contador[key] += int(value)
-        else:
-         contador[key] = int(value)
-
-
-    contador = [(key,contador[key]) for key in contador]
-    contador
-
-    from operator import itemgetter
-    contador.sort(key=itemgetter(0), reverse=False)  #ordenar de acuerdo a llave
     
-    return contador
+    return 
 
 def pregunta_04():
     """
@@ -122,26 +93,8 @@ def pregunta_04():
     ]
 
     """
-    with open("data.csv", "r") as file:
-    datos = file.readlines() 
-
-    datos = [row.replace("\n", "")for row in datos]
-    datos = [row.split("\t")for row in datos]
-
-    datos1 = [row[2] for row in datos]
-
-    datos1 = [line.split(",") for line in datos1] 
-    datos1 = [z[0].split("-")[1] for z in datos1[0:]] 
-
-    from collections import Counter
-
-    resultado = Counter(datos1)
-    resultado = list(resultado.items()) #convertir diccionario a listas de tuplas
-
-    from operator import itemgetter
-    resultado.sort(key=itemgetter(0), reverse=False)  #ordenar de acuerdo a llave
     
-    return resultado
+    return 
 
 
 def pregunta_05():
